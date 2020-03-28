61c4ee1947fa1ecda99054c2c3d0f6945c5f1993
Ray Alva <rayalva407@gmail.com>
2020-03-28 23:24:51 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  while i < colletion.validates_length
+  while i < collection.validates_length
     return collection[i] if yield(collection[i])
     i = i + 1
   end
